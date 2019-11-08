cc7c4d58ba82f28f593ddf502d6e697f0b3582e4
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2019-11-08 17:31:55 +0100

    Work on firmware.

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,6 +3,11 @@
 
 use core::panic::PanicInfo;
 use stm32f2::stm32f215;
+use cortex_m;
+use heapless;
+
+static mut USART1_QUEUE: heapless::spsc::Queue<u8, heapless::consts::U128> =
+    heapless::spsc::Queue(heapless::i::Queue::new());
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
@@ -13,6 +18,7 @@ fn panic(_: &PanicInfo) -> ! {
         // Here we just blink the red LED forever to indicate there is a
         // problem.
         let peripherals = stm32f215::Peripherals::steal();
+        set_led_green(&peripherals, false);
         loop {
             for _ in 0..50000 {
                 set_led_red(&peripherals, true);
@@ -24,8 +30,25 @@ fn panic(_: &PanicInfo) -> ! {
     }
 }
 
-pub extern "C" fn default_handler() {
-    loop {}; 
+pub extern "C" fn handler_default() {
+    loop {};
+}
+
+/// USART interrupt handler. Called in case of data byte reception or overrun.
+/// When a byte is received, it is pushed in the USART queue. If the queue is
+/// full, the program will panic.
+pub extern "C" fn handler_usart1() {
+    unsafe {
+        let mut producer = USART1_QUEUE.split().0;
+        let peripherals = stm32f215::Peripherals::steal();
+        if peripherals.USART1.sr.read().rxne().bit() {
+            // If queue is full, panic!
+            producer.enqueue(peripherals.USART1.dr.read().bits() as u8).unwrap();
+        } else {
+            // This is probably an overrun error.
+            panic!();
+        }
+    }
 }
 
 #[link_section=".isr_vectors.reset"]
@@ -34,7 +57,11 @@ pub static reset_vector: unsafe extern "C" fn() -> ! = _start;
 
 #[link_section=".isr_vectors"]
 #[no_mangle]
-pub static interrupt_vectors: [unsafe extern "C" fn(); 79] = [default_handler; 79];
+pub static interrupt_vectors: [unsafe extern "C" fn(); 95] = {
+    let mut v: [unsafe extern "C" fn(); 95] = [handler_default; 95];
+    v[51] = handler_usart1;
+    v
+};
 
 /// Toggle the red LED on or off.
 /// `peripherals` - This method needs to borrow the peripherals.
@@ -48,6 +75,43 @@ fn set_led_red(peripherals: &stm32f215::Peripherals, state: bool) {
 /// `state` - true to turn on the LED, false to turn off.
 fn set_led_green(peripherals: &stm32f215::Peripherals, state: bool) {
     peripherals.GPIOC.odr.modify(|_, w| { w.odr14().bit(state) });
+}
+
+/// Enable or disable on-board 15 V regulator.
+/// `peripherals` - This method needs to borrow the peripherals.
+/// `state` - true to turn on the regulator, false to turn off.
+fn set_15v_regulator(peripherals: &stm32f215::Peripherals, state: bool) {
+    peripherals.GPIOB.odr.modify(|_, w| { w.odr11().bit(state) });
+}
+
+/// Approximated delay function. Precise enought for what we need to do...
+#[inline(never)]
+fn delay_ms(duration: u32) {
+    // Estimated duration for each loop: 7 clock cycles.
+    let count: u32 = (duration * 16000) / 7;
+    for _ in 0..count {
+        cortex_m::asm::nop();
+    }
+}
+
+/// Receives a byte from USART1. Blocks until data is available.
+fn usart1_rx() -> u8 {
+    unsafe {
+        let mut producer = USART1_QUEUE.split().1;
+        loop {
+            match producer.dequeue() {
+                Some(byte) => { return byte; }
+                None => {}
+            }
+        }
+    }
+}
+
+/// Transmits a byte over USART1.
+/// `peripherals` - This method needs to borrow the peripherals.
+/// `value` - Byte to be transmitted.
+fn usart1_tx(peripherals: &stm32f215::Peripherals, value: u8) {
+    peripherals.USART1.dr.write(|w| { w.dr().bits(value as u16) });
 }
 
 #[no_mangle]
@@ -64,20 +128,24 @@ pub extern "C" fn _start() -> ! {
             core::ptr::write_volatile(i as *mut u32, 0u32);
         }
     }
+
     let peripherals = stm32f215::Peripherals::take().unwrap();
 
     peripherals.RCC.apb2enr.write(|w| { w.usart1en().set_bit() });
     // USART1 uses PA9 for TX and PA10 for RX.
     // LEDs are connected to PC13 and PC14.
-    // Enable clock for PORT A and PORT C peripherals.
+    // Enable clock for PORT A, PORT B and PORT C peripherals.
     peripherals.RCC.ahb1enr.write(
-        |w| { w.gpioaen().set_bit().gpiocen().set_bit() } );
+        |w| { w.gpioaen().set_bit().gpioben().set_bit().gpiocen().set_bit() } );
     peripherals.GPIOC.moder.write(|w| { w.moder13().bits(1).moder14().bits(1) });
+    set_15v_regulator(&peripherals, false);
+    peripherals.GPIOB.moder.write(|w| { w.moder11().bits(1) });
 
     // Configure UART1
     // UART Enable, Transmitter Enable, Receiver Enable
     peripherals.USART1.cr1.write(
         |w| { w.ue().set_bit().te().set_bit().re().set_bit() });
+    peripherals.USART1.cr2.write(|w|{ w.stop().bits(2) });
     // Baudrate is Fck/(8*(2-OVER8)*DIV)
     // Fck = 16 MHz (High Speed Internal oscillator)
     // OVER8 = 0
@@ -94,11 +162,30 @@ pub extern "C" fn _start() -> ! {
     // Configure PA9 and PA10 GPIOs in high frequency
     peripherals.GPIOA.ospeedr.write(
         |w| { w.ospeedr10().bits(3).ospeedr9().bits(3) });
+    // Enable interrupt for USART1
+    peripherals.USART1.cr1.modify(|_, w| { w.rxneie().set_bit() });
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(stm32f215::Interrupt::USART1);
+    }
 
+    // Give some time for the FT232 to boot-up.
+    delay_ms(500);
     set_led_green(&peripherals, true);
 
-    loop {
-        unsafe { peripherals.USART1.dr.write(|w| { w.bits('b' as u32) }); }
-        //USART.tx_str("Hello world!\nI'm a Rustacean!\n");
+    loop
+    {
+        let command_byte = usart1_rx();
+        match command_byte {
+            0x01 => {
+                let value = usart1_rx();
+                assert!(value <= 1);
+                set_15v_regulator(&peripherals, value != 0);
+                usart1_tx(&peripherals, command_byte);
+            }
+            _ => {
+                // Unknown command. Panic!
+                panic!();
+            }
+        }
     }
 }
