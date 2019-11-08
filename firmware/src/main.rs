50785cf0ba3b69884480d7c5bb2ebf8d14ee98c3
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2019-11-08 12:00:15 +0100

    Clean-up and improvements in firmware.

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -6,7 +6,22 @@ use stm32f2::stm32f215;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
-    loop {}
+    unsafe {
+        // In case of panic, the peripherals may have already been taken, and we
+        // cannot take it again... But this is panic, we can do dirty things and
+        // call steal to use peripherals anyway!
+        // Here we just blink the red LED forever to indicate there is a
+        // problem.
+        let peripherals = stm32f215::Peripherals::steal();
+        loop {
+            for _ in 0..50000 {
+                set_led_red(&peripherals, true);
+            }
+            for _ in 0..50000 {
+                set_led_red(&peripherals, false);
+            }
+        }
+    }
 }
 
 pub extern "C" fn default_handler() {
@@ -21,12 +36,19 @@ pub static reset_vector: unsafe extern "C" fn() -> ! = _start;
 #[no_mangle]
 pub static interrupt_vectors: [unsafe extern "C" fn(); 79] = [default_handler; 79];
 
-const RCC_AHB1ENR: u32 = 0x40023830;
-const GPIOC_BASE: u32 = 0x40020800;
-const GPIOC_MODER: u32 = GPIOC_BASE + 0x00;
-const GPIOC_OSPEEDR: u32 = GPIOC_BASE + 0x08;
-const GPIOC_ODR: u32 = GPIOC_BASE + 0x14;
+/// Toggle the red LED on or off.
+/// `peripherals` - This method needs to borrow the peripherals.
+/// `state` - true to turn on the LED, false to turn off.
+fn set_led_red(peripherals: &stm32f215::Peripherals, state: bool) {
+    peripherals.GPIOC.odr.modify(|_, w| { w.odr13().bit(state) });
+}
 
+/// Toggle the red LED on or off.
+/// `peripherals` - This method needs to borrow the peripherals.
+/// `state` - true to turn on the LED, false to turn off.
+fn set_led_green(peripherals: &stm32f215::Peripherals, state: bool) {
+    peripherals.GPIOC.odr.modify(|_, w| { w.odr14().bit(state) });
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -43,19 +65,6 @@ pub extern "C" fn _start() -> ! {
         }
     }
     let peripherals = stm32f215::Peripherals::take().unwrap();
-    /*unsafe {
-        peripherals.RCC.ahb1enr.write(|w| { w.bits(1 << 2) });
-        peripherals.GPIOC.moder.write(|w| { w.bits(1 << 26) });
-        peripherals.GPIOC.ospeedr.write(|w| { w.bits(3 << 26) });
-        loop {
-            for i in 0..200000 {
-                peripherals.GPIOC.odr.write(|w| { w.bits(0) });
-            }
-            for i in 0..200000 {
-                peripherals.GPIOC.odr.write(|w| { w.bits(1 << 13) });
-            }
-        }
-    }*/
 
     peripherals.RCC.apb2enr.write(|w| { w.usart1en().set_bit() });
     // USART1 uses PA9 for TX and PA10 for RX.
@@ -63,9 +72,7 @@ pub extern "C" fn _start() -> ! {
     // Enable clock for PORT A and PORT C peripherals.
     peripherals.RCC.ahb1enr.write(
         |w| { w.gpioaen().set_bit().gpiocen().set_bit() } );
-
     peripherals.GPIOC.moder.write(|w| { w.moder13().bits(1).moder14().bits(1) });
-    peripherals.GPIOC.odr.write(|w| { w.odr13().set_bit().odr14().set_bit() });
 
     // Configure UART1
     // UART Enable, Transmitter Enable, Receiver Enable
@@ -87,6 +94,8 @@ pub extern "C" fn _start() -> ! {
     // Configure PA9 and PA10 GPIOs in high frequency
     peripherals.GPIOA.ospeedr.write(
         |w| { w.ospeedr10().bits(3).ospeedr9().bits(3) });
+
+    set_led_green(&peripherals, true);
 
     loop {
         unsafe { peripherals.USART1.dr.write(|w| { w.bits('b' as u32) }); }
