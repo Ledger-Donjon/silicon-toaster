540edd1f99462d338d86d2832c850dd105148e8e
Michael Mouchous <michael.mouchous@ledger.fr>
2022-09-15 13:40:07 +0200

    Script to measure the resulting voltage for a given width/period PWM settings

---
+++ calibrate_voltage.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+This script is to perform measures on the obtained value of "read_voltage()" after a certain
+time, according to 'period' and 'width' parameters.
+"""
+import numpy
+from silicontoaster import SiliconToaster
+import datetime
+from time import sleep
+import quicklog
+
+toaster = SiliconToaster("/dev/ttyUSB3")
+toaster.on()
+
+avg_count = 100
+
+log = quicklog.Log(f"calibration.log")
+already_done = []
+for record in quicklog.read_log(f"calibration.log"):
+    already_done.append((float(record["width"]), float(record["period"])))
+
+for period in list(range(100, 1600, 50))[::-1]:
+    for width in range(1, 25):
+        print(f"Measuring for period {period}, width {width}...")
+        if (width, period) in already_done:
+            print(f"Already done, skipping.")
+            continue
+        toaster.set_pwm_settings(period=period, width=width)
+
+        # The array containing the read values.
+        voltages = []
+        # For tracking time.
+        last_print_time = start = datetime.datetime.now()
+        # To compute the standard deviation
+        stddev = 0.0
+        # We prevent the measurement to last indefinitely
+        while (now := datetime.datetime.now()) - start < datetime.timedelta(minutes=2):
+            # Read the voltage and append it
+            voltages.append(toaster.read_voltage())
+            # We keep only the last values
+            voltages = voltages[-avg_count:]
+            if len(voltages) == avg_count:
+                stddev = numpy.std(voltages)
+                if stddev < 1.5:
+                    # The standard deviation of the last measurements is low:
+                    # We consider that the reading of the voltage is stable enough
+                    break
+                if last_print_time - now > datetime.timedelta(seconds=20):
+                    print(stddev)
+                    last_print_time = now
+            # Each measurement is delayed
+            sleep(0.05)
+
+        record = {
+            "width": width,
+            "voltage": numpy.average(voltages),
+            "period": period,
+            "std": stddev,
+            "time": int((now - start).total_seconds()),
+        }
+        print(record)
+        log.append(record)
+        log.flush()
+        if numpy.average(voltages) > 950:
+            break
---
+++ plot-calibration_voltage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import quicklog
+import numpy as np
+
+x = []
+y = []
+z = []
+
+for record in quicklog.read_log(f"calibration.log"):
+    x.append(float(record["period"]))
+    y.append(float(record["width"]))
+    z.append(float(record["voltage"]))
+X, Y, Z = [np.array(i) for i in [x, y, z]]
+ax = plt.axes(projection="3d")
+surf = ax.plot_trisurf(x, y, z, cmap="cool", edgecolor="black")
+surf.set_linewidth(0.1)
+ax.set_title("Voltage obtained according PDM width/period")
+ax.set_xlabel("Period")
+ax.set_ylabel("Width")
+ax.set_zlabel("Voltage (V)")
+plt.colorbar(surf)
+plt.show()
