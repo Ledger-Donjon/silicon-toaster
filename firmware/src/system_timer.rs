763851b36d8afd1a0f2f7b34d6a742eea1ae0c2c
Michael Mouchous <michael.mouchous@ledger.fr>
2022-09-28 16:36:21 +0200

    SystemTimer

---
+++ firmware/src/system_timer.rs
@@ -0,0 +1,49 @@
+use stm32f2::stm32f215;
+
+pub struct SystemTimer<'a> {
+    stk: &'a stm32f215::STK,
+    reload_counts: u32,
+}
+
+impl<'a> SystemTimer<'a> {
+    pub const FREQ: u64 = 8047640;
+
+    pub fn new(stk: &'a stm32f215::STK) -> SystemTimer {
+        let sys_timer = SystemTimer {
+            stk,
+            reload_counts: 0,
+        };
+        unsafe {
+            // Initialize the Reload Value Register to the max.
+            stk.load_.write(|w| w.bits(0x00FF_FFFF));
+            // Enable the timer, by setting the ENABLE bit (bit 0) in Control and Status register
+            stk.ctrl.modify(|_, w| w.enable().set_bit());
+        };
+        sys_timer
+    }
+
+    // This function must be called regularly to count the reloads operation
+    // by evaluating the COUNTFLAG bit of Control and Status Register.
+    fn check_reloads(&mut self) {
+        // The Control and Status Register's COUNTFLAG (bit 16)
+        // Returns 1 if timer counted to 0 since last time this was read.
+        // Meaning that a reload has been done in the Current Value Register.
+        if self.stk.ctrl.read().countflag().bit() {
+            self.reload_counts += 1;
+        }
+    }
+
+    pub fn get_ticks(&mut self) -> u64 {
+        // Get the time as number of ticks, considering the counts of the reloads.
+        self.check_reloads();
+        let reload = self.stk.load_.read().reload().bits() as u64;
+        let remained_ticks = reload - (self.stk.val.read().bits() as u64);
+        return remained_ticks + reload * (self.reload_counts as u64);
+    }
+
+    pub fn get_time(&mut self) -> f64 {
+        // Get the time as seconds
+        // It has been estimated that 8047640 ticks corresponds to 1 second.
+        self.get_ticks() as f64 / SystemTimer::FREQ as f64
+    }
+}
