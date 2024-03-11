fad1e438a83915cb2e0db0a86a0caf115f87b517
Michael Mouchous <michael.mouchous@ledger.fr>
2024-03-11 11:17:09 +0100

    Include GUI in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="silicon-toaster",
     version="1.1",
-    packages=["silicontoaster"],
+    packages=["silicontoaster", "silicontoaster.gui"],
     install_requires=["pyserial"],
     url="",
     license="GNU LGPL",
